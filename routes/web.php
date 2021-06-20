3fd6eaee369061c799d9fbb344f9d4292ee18732
kimjin0 <pkimjin0q@gmail.com>
2021-06-20 14:54:29 +0900

    laravel Tutorial - factory까지 실습

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\HomeController;
+use App\http\Controllers\PostsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/data', [HomeController::class, 'index'])->name('data');
+Route::get('/notice', [HomeController::class, 'index'])->name('notice');
 
-Route::get('/', 'HomeController@index');
-Route::get('/data', 'HomeController@data');
-Route::get('/notice', 'HomeController@notice');
+// Route::get('/', 'HomeController@index');
+// Route::get('/data', 'HomeController@data');
+// Route::get('/notice', 'HomeController@notice');
+
+//Posts endPoint
+Route::get('/posts', [ PostsController::class, 'index']);
